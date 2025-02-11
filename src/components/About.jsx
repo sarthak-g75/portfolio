cabafea844051030a7edde560df361d22238274f
Sarthak Gupta <gsarthak783891@gmail.com>
2025-02-11 18:30:59 +0530

    added sub title to contact page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,11 +5,11 @@ import AboutImage from './AboutImage'
 const About = () => {
   return (
     <div
-      className='flex flex-col items-center w-full min-h-screen scroll-mt-10'
+      className='flex flex-col items-center w-full pb-10 scroll-mt-10'
       id='about'
     >
       <SubTitle title='About' />
-      <div className='flex flex-col items-center justify-center w-full pb-10'>
+      <div className='flex flex-col items-center justify-center w-full h-full mt-10'>
         <div className='flex flex-col items-center w-full gap-10 px-4 2xl:gap-40 lg:px-14 2xl:justify-center lg:justify-between lg:gap-20 md:flex-row'>
           <AboutInroSection />
           <AboutImage />
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,7 +1,10 @@
-import React from 'react'
-
+import SubTitle from '../components/SubTitle'
 const Contact = () => {
-  return <div className='h-screen'>Contact</div>
+  return (
+    <div className='h-screen'>
+      <SubTitle title='Contact ' />
+    </div>
+  )
 }
 
 export default Contact
