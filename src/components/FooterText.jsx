e0865aed66b10617729bcf75a5cf90b3c7a852c1
Sarthak Gupta <gsarthak783891@gmail.com>
2025-11-13 22:48:38 +0530

    update

--- src/components/FooterText.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-const text =
-  'Got a big idea brewing? I’m open for new projects! If you need a developer who brings ideas to life (and asks all the right, sometimes weird, questions), let’s chat! 🚀'
-const FooterText = () => {
-  return <h2 className='2xl:text-4xl lg:text-2xl'>{text}</h2>
-}
-
-export default FooterText
